607327a8d5126b266d05aaa96fb0d283febb9935
bisbiss <01newbi@gmail.com>
2025-01-23 11:51:56 +0700

    update homepage add filter to widget stats

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,6 +2,8 @@
 
 namespace App\Filament\Widgets;
 
+use Carbon\Carbon;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Post;
@@ -10,18 +12,28 @@ use App\Models\User;
 
 class StatsOverview extends BaseWidget
 {
+    use InteractsWithPageFilters;
+
     protected function getStats(): array
     {
+        $startDate = ! is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = ! is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
         // get count of posts
-        $posts = Post::count();
+        $posts = Post::get()->whereBetween('created_at', [$startDate, $endDate]);
         // get count of categories
-        $categories = Category::count();
+        $categories = Category::get()->whereBetween('created_at', [$startDate, $endDate]);
         // get count of users
-        $users = User::count();
+        $users = User::get()->whereBetween('created_at', [$startDate, $endDate]);
         return [
-            Stat::make('Posts', $posts),
-            Stat::make('Categories', $categories),
-            Stat::make('Users', $users),
+            Stat::make('Posts', number_format(count($posts))),
+            Stat::make('Categories', count($categories)),
+            Stat::make('Users', count($users)),
         ];
     }
 }
--- app/Filament/Widgets/WidgetPostsChart.php
+++ app/Filament/Widgets/WidgetPostsChart.php
@@ -11,11 +11,9 @@ class WidgetPostsChart extends ChartWidget
 {
     protected static ?string $heading = 'Posts Chart';
     protected static string $color = 'info';
-    public ?string $filter = 'today';
 
     protected function getData(): array
     {
-        $activeFilter = $this->filter;
         $data = Trend::model(Post::class)
             ->between(
                 start: now()->startOfYear(),
@@ -38,15 +36,5 @@ class WidgetPostsChart extends ChartWidget
     protected function getType(): string
     {
         return 'line';
-    }
-
-    protected function getFilters(): ?array
-    {
-        return [
-            'today' => 'Today',
-            'week' => 'Last week',
-            'month' => 'Last month',
-            'year' => 'This year',
-        ];
     }
 }
